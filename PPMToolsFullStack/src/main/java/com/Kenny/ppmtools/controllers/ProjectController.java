c3c7a0c28b0bd284c5e06a97deef9fe0af000cd1
Kenny Lam <kinwai.lam730@gmail.com>
2020-06-08 17:51:09 -0400

    Added a ValidationService class

--- PPMToolsFullStack/src/main/java/com/Kenny/ppmtools/controllers/ProjectController.java
+++ PPMToolsFullStack/src/main/java/com/Kenny/ppmtools/controllers/ProjectController.java
@@ -2,13 +2,17 @@ package com.Kenny.ppmtools.controllers;
 
 import com.Kenny.ppmtools.domain.Project;
 import com.Kenny.ppmtools.services.ProjectService;
+import com.Kenny.ppmtools.services.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,8 +21,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private ValidationService validationService;
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+        ResponseEntity<?> errorMap = validationService.MapValidationService(result);
+        if(errorMap != null) return errorMap;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
