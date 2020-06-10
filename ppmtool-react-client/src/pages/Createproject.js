1a259ed00df57c0162d3c081e8b333631eaedae6
Kenny Lam <kinwai.lam730@gmail.com>
2020-06-10 16:29:30 -0400

    Made the layout of create project page so far

--- ppmtool-react-client/src/pages/Createproject.js
+++ ppmtool-react-client/src/pages/Createproject.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Input, Grid, TextField, Card, Typography } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    cardStyle:{
+      width: "80%",
+      marginTop: theme.spacing(6)
+    },
+    textFieldStyle:{
+      width: "90%",
+    },
+    formStyle:{
+      alignItems: "center",
+      textAlign: "center"
+    },
+    typographyStyle:{
+      textAlign: "center"
+    }
+}))
 
 export default function Createproject(){
+
+  const classes = useStyles();
+  const theme = useTheme();
+
   return(
     <>
-      <h1>Create a new project</h1>
+      <Grid container direction="column" justify="flex-start" alignItems="center" spacing={1}>
+        <Card className={classes.cardStyle}>
+          <Typography className={classes.typographyStyle}><h1>Create Project</h1></Typography>
+          <form noValidate autoComplete="off" className={classes.formStyle}>
+            <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project Name" style={{ margin: 8 }} placeholder="Project Name" margin="normal" variant="outlined" />
+            <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project Name" style={{ margin: 8 }} placeholder="Project Name" margin="normal" variant="outlined" />
+            <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project Name" style={{ margin: 8 }} placeholder="Project Name" margin="normal" variant="outlined" />
+            <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project Name" style={{ margin: 8, marginBottom: "2em" }} placeholder="Project Name" margin="normal" variant="outlined" />
+          </form>
+        </Card>
+      </Grid>
     </>
   )
 }
