e89a8b18f1cd921a761de5a1abb7c64dac8ce1c4
Kenny Lam <kinwai.lam730@gmail.com>
2020-06-10 17:19:05 -0400

    Added a submit button with styling

--- ppmtool-react-client/src/pages/Createproject.js
+++ ppmtool-react-client/src/pages/Createproject.js
@@ -1,5 +1,5 @@
 import React, {useRef} from 'react'
-import { Grid, TextField, Card, Typography } from '@material-ui/core';
+import { Grid, TextField, Card, Typography, Button } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -22,6 +22,10 @@ const useStyles = makeStyles((theme) => ({
     },
     typographyStyle:{
       textAlign: "center"
+    },
+    buttonStyle:{
+      width: "90%",
+      padding: "1em"
     }
 }))
 
@@ -42,11 +46,12 @@ export default function Createproject(){
         <Card className={classes.cardStyle}>
           <Typography className={classes.typographyStyle}><h1>Create Project</h1></Typography>
           <form noValidate autoComplete="off" className={classes.formStyle}>
-              <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project Name" style={{ margin: 10 }} placeholder="Project Name" margin="normal" variant="outlined" ref={projectName} />
-              <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project ID" style={{ margin: 10 }} placeholder="Project ID" margin="normal" variant="outlined" ref={projectId}/>
-              <TextField className={classes.textFieldStyle} placeholder="MultiLine with rows: 2 and rowsMax: 4" multiline rows={2} rowsMax={4} id="outlined-full-width" label="Project Description" style={{ margin: 10 }} placeholder="Project Description" margin="normal" variant="outlined" ref={projectDesc} />
-              <TextField className={classes.textDateFieldStyle} id="outlined-full-width" label="Start Date" type="Date" style={{ margin: 15 }} placeholder="Project Name" margin="normal" variant="outlined" InputLabelProps={{ shrink: true }} ref={projectStart} />
-              <TextField className={classes.textDateFieldStyle} id="outlined-full-width" label="End Date" type="Date" style={{ margin: 15, marginBottom: "3em"}} placeholder="Project Name" margin="normal" variant="outlined" InputLabelProps={{ shrink: true }} ref={projectEnd} />
+            <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project Name" style={{ margin: 10 }} placeholder="Project Name" margin="normal" variant="outlined" ref={projectName} />
+            <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project ID" style={{ margin: 10 }} placeholder="Project ID" margin="normal" variant="outlined" ref={projectId}/>
+            <TextField className={classes.textFieldStyle} placeholder="MultiLine with rows: 2 and rowsMax: 4" multiline rows={2} rowsMax={4} id="outlined-full-width" label="Project Description" style={{ margin: 10 }} placeholder="Project Description" margin="normal" variant="outlined" ref={projectDesc} />
+            <TextField className={classes.textDateFieldStyle} id="outlined-full-width" label="Start Date" type="Date" style={{ margin: 15 }} placeholder="Project Name" margin="normal" variant="outlined" InputLabelProps={{ shrink: true }} ref={projectStart} />
+            <TextField className={classes.textDateFieldStyle} id="outlined-full-width" label="End Date" type="Date" style={{ margin: 15}} placeholder="Project Name" margin="normal" variant="outlined" InputLabelProps={{ shrink: true }} ref={projectEnd} />
+            <Button className={classes.buttonStyle} style={{ margin: 15, marginBottom: "3em"}} variant="contained" color="primary">Submit</Button>
           </form>
         </Card>
       </Grid>
