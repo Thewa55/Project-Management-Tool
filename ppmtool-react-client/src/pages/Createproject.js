87aa663aa9345de481e6c2832621b029c9d1efdd
Kenny Lam <kinwai.lam730@gmail.com>
2020-06-10 17:08:16 -0400

    Added useref to all input fields

--- ppmtool-react-client/src/pages/Createproject.js
+++ ppmtool-react-client/src/pages/Createproject.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { Grid, TextField, Card, Typography } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
@@ -30,17 +30,23 @@ export default function Createproject(){
   const classes = useStyles();
   const theme = useTheme();
 
+  const projectName = useRef();
+  const projectId = useRef();
+  const projectDesc = useRef();
+  const projectStart = useRef();
+  const projectEnd = useRef();
+
   return(
     <>
       <Grid container direction="column" justify="flex-start" alignItems="center" spacing={1}>
         <Card className={classes.cardStyle}>
           <Typography className={classes.typographyStyle}><h1>Create Project</h1></Typography>
           <form noValidate autoComplete="off" className={classes.formStyle}>
-              <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project Name" style={{ margin: 10 }} placeholder="Project Name" margin="normal" variant="outlined" />
-              <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project ID" style={{ margin: 10 }} placeholder="Project ID" margin="normal" variant="outlined" />
-              <TextField className={classes.textFieldStyle} placeholder="MultiLine with rows: 2 and rowsMax: 4" multiline rows={2} rowsMax={4} id="outlined-full-width" label="Project Description" style={{ margin: 10 }} placeholder="Project Description" margin="normal" variant="outlined"  />
-              <TextField className={classes.textDateFieldStyle} id="outlined-full-width" label="Start Date" type="Date" style={{ margin: 15 }} placeholder="Project Name" margin="normal" variant="outlined" InputLabelProps={{ shrink: true }} />
-              <TextField className={classes.textDateFieldStyle} id="outlined-full-width" label="End Date" type="Date" style={{ margin: 15, marginBottom: "3em"}} placeholder="Project Name" margin="normal" variant="outlined" InputLabelProps={{ shrink: true }} />
+              <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project Name" style={{ margin: 10 }} placeholder="Project Name" margin="normal" variant="outlined" ref={projectName} />
+              <TextField className={classes.textFieldStyle} id="outlined-full-width" label="Project ID" style={{ margin: 10 }} placeholder="Project ID" margin="normal" variant="outlined" ref={projectId}/>
+              <TextField className={classes.textFieldStyle} placeholder="MultiLine with rows: 2 and rowsMax: 4" multiline rows={2} rowsMax={4} id="outlined-full-width" label="Project Description" style={{ margin: 10 }} placeholder="Project Description" margin="normal" variant="outlined" ref={projectDesc} />
+              <TextField className={classes.textDateFieldStyle} id="outlined-full-width" label="Start Date" type="Date" style={{ margin: 15 }} placeholder="Project Name" margin="normal" variant="outlined" InputLabelProps={{ shrink: true }} ref={projectStart} />
+              <TextField className={classes.textDateFieldStyle} id="outlined-full-width" label="End Date" type="Date" style={{ margin: 15, marginBottom: "3em"}} placeholder="Project Name" margin="normal" variant="outlined" InputLabelProps={{ shrink: true }} ref={projectEnd} />
           </form>
         </Card>
       </Grid>
